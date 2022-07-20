0942a26efff05f935d577fd345af9fee124d0e18
Ethan Olsen <ethan@crowdhubapps.com>
2022-07-19 23:42:57 -0700

    chore: update about app styling

--- src/lib/aboutApp/AboutApp.svelte
+++ src/lib/aboutApp/AboutApp.svelte
@@ -9,10 +9,10 @@
 </script>
 
 <Window
-	startingHeight="360px"
+	startingHeight="332px"
 	startingWidth="560px"
-	headerClass="!bg-[rgb(48,45,52)] !shadow"
-	windowClass="!ring-[2px] !ring-inset !ring-white/50 !border-[rgb(5,9,12)] !border"
+	headerClass="!bg-[rgb(48,45,51)] !shadow !border-b !border-black"
+	windowClass="!ring-[2px] !ring-inset !ring-[rgb(5,9,12)] !border-white/10 !border !resize-none"
 	{appWindow}
 >
 	<div slot="header" class="flex w-full items-center justify-center pr-14">
@@ -22,7 +22,7 @@
 			{#each tabs as item, i}
 				<button
 					on:click|preventDefault|stopPropagation={() => (activeTab = item)}
-					class="flex rounded px-1 text-center text-slate-500 {activeTab === item
+					class="flex rounded px-1 text-center text-sm text-slate-500 {activeTab === item
 						? 'bg-slate-700 text-white'
 						: ''}"
 				>
@@ -34,9 +34,21 @@
 			{/each}
 		</div>
 	</div>
-	<div class="h-full w-full bg-[rgb(34,28,38)]">
+	<div class="flex h-full w-full flex-col bg-[rgb(29,26,37)]">
 		{#if activeTab === 'Overview'}
-			<h1>Overview</h1>
+			<div class="flex flex-grow items-center justify-center gap-12">
+				<div
+					class="h-32 w-32 rounded-full bg-gradient-to-tr from-purple-900 to-purple-700 ring-[0.375rem] ring-white"
+				/>
+				<div class="flex gap-4">
+					<div class="flex flex-col">
+						<h1 class="text-3xl font-medium">
+							olsenOS <span class="font-extralight">Monterey</span>
+						</h1>
+						<p class="text-xs font-light">Version 1.0</p>
+					</div>
+				</div>
+			</div>
 		{:else if activeTab === 'About'}
 			<h1>About</h1>
 		{:else if activeTab === 'Contact'}
