eabf0663b9494ab81183d5fb29df42a1418ad607
Ethan Olsen <ethan@crowdhubapps.com>
2022-07-20 22:26:54 -0700

    chore: add fade in and out for about pages

--- src/lib/aboutApp/AboutApp.svelte
+++ src/lib/aboutApp/AboutApp.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import EOSIcon from './EOSIcon.svelte';
 	import Window from '$lib/window/Window.svelte';
 	import type { AppWindow } from '$stores/apps.store';
@@ -24,69 +25,88 @@
 				<button
 					on:click|preventDefault|stopPropagation={() => (activeTab = item)}
 					class="flex rounded px-1 text-center text-sm text-slate-500 {activeTab === item
-						? 'bg-slate-700 text-white'
+						? 'bg-[#3F3C44] text-white'
 						: ''}"
 				>
 					{item}
 				</button>
 				{#if i < 2}
-					<div class="h-3 w-[1px] bg-slate-500" />
+					<div class="h-3 w-[1px] bg-[#3F3C44]" />
 				{/if}
 			{/each}
 		</div>
 	</div>
-	<div class="flex h-full w-full flex-col bg-[rgb(29,26,37)]">
+	<div class="relative flex h-full w-full flex-col bg-[#1E1B24]">
 		{#if activeTab === 'Overview'}
-			<div class="flex flex-grow items-center justify-center gap-12">
-				<EOSIcon />
-				<div class="flex gap-4">
-					<div class="flex flex-col gap-4">
-						<div>
-							<h1 class="text-3xl font-medium">
-								olsenOS <span class="font-extralight">Monterey</span>
-							</h1>
-							<p class="text-xs">Version <span class="font-light">1.0</span></p>
-						</div>
-						<div class="flex flex-col gap-0.5">
-							<p class="text-xs">Ethan Olsen Dev (2022)</p>
-							<div class="flex gap-2 text-xs">
-								<p>Developer</p>
-								<p class="font-light">Full-Stack</p>
+			<div
+				in:fade={{ duration: 100 }}
+				out:fade={{ duration: 100 }}
+				class="absolute flex h-full w-full flex-grow flex-col"
+			>
+				<div class="flex flex-grow items-center justify-center gap-12">
+					<EOSIcon />
+					<div class="flex gap-4">
+						<div class="flex flex-col gap-4">
+							<div>
+								<h1 class="text-3xl font-medium">
+									olsenOS <span class="font-extralight">Mint</span>
+								</h1>
+								<p class="text-xs">Version <span class="font-light">1.0</span></p>
 							</div>
-							<div class="flex gap-2 text-xs">
-								<p>Chip</p>
-								<p class="font-light">Pringles</p>
+							<div class="flex flex-col gap-0.5">
+								<p class="text-xs">Ethan Olsen Dev (2022)</p>
+								<div class="flex gap-2 text-xs">
+									<p>Developer</p>
+									<p class="font-light">Full-Stack</p>
+								</div>
+								<div class="flex gap-2 text-xs">
+									<p>Chip</p>
+									<p class="font-light">Pringles</p>
+								</div>
+								<div class="flex gap-2 text-xs">
+									<p>Memory</p>
+									<p class="font-light">Pretty good</p>
+								</div>
+								<div class="flex gap-2 text-xs">
+									<p>Cereal Number</p>
+									<p class="font-light">A lot I guess</p>
+								</div>
 							</div>
-							<div class="flex gap-2 text-xs">
-								<p>Memory</p>
-								<p class="font-light">Pretty good</p>
+							<div class="flex w-full gap-2">
+								<button
+									class="flex-grow rounded bg-[#56545C] py-[1px] px-1.5 text-xs text-white"
+									type="button"
+								>
+									System Report
+								</button>
+								<button
+									class="flex-grow rounded bg-[#56545C] py-[1px] px-1.5 text-xs text-white"
+									type="button"
+								>
+									Software Update
+								</button>
 							</div>
-							<div class="flex gap-2 text-xs">
-								<p>Cereal Number</p>
-								<p class="font-light">A lot I guess</p>
-							</div>
-						</div>
-						<div class="flex w-full gap-2">
-							<button
-								class="flex-grow rounded bg-[rgb(86,83,90)] py-[1px] px-1.5 text-xs text-white"
-								type="button"
-							>
-								System Report
-							</button>
-							<button
-								class="flex-grow rounded bg-[rgb(86,83,90)] py-[1px] px-1.5 text-xs text-white"
-								type="button"
-							>
-								Software Update
-							</button>
 						</div>
 					</div>
 				</div>
+				<p class="pb-1 text-center text-[10px] text-white/20">2022 Ethan Olsen Development</p>
 			</div>
 		{:else if activeTab === 'About'}
-			<h1>About</h1>
+			<div
+				in:fade={{ duration: 100 }}
+				out:fade={{ duration: 100 }}
+				class="absolute flex h-full w-full flex-grow flex-col"
+			>
+				<h1>About</h1>
+			</div>
 		{:else if activeTab === 'Contact'}
-			<h1>Contact</h1>
+			<div
+				in:fade={{ duration: 100 }}
+				out:fade={{ duration: 100 }}
+				class="absolute flex h-full w-full flex-grow flex-col"
+			>
+				<h1>Contact</h1>
+			</div>
 		{/if}
 	</div>
 </Window>
