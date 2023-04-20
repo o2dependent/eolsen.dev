773540d30e1f43b260157353f6f58f1fb460f839
Ethan Olsen <131eolsen@gmail.com>
2023-04-20 02:35:13 -0700

    chore: add contact panel base

---
+++ src/lib/aboutApp/panels/ContactPanel.svelte
@@ -0,0 +1,27 @@
+<div class="grid flex-grow items-center justify-center gap-12 px-8">
+	<div class="flex h-full flex-col gap-4 py-8">
+		<div class="flex flex-grow flex-col gap-4 overflow-hidden">
+			<div>
+				<h1 class="text-3xl font-medium">
+					<span class="font-extralight">Contact</span> Me
+				</h1>
+				<a class="text-sm font-light" href="mailto:ethan@eolsen.dev">ethan@eolsen.dev</a>
+			</div>
+			<form class="relative h-full flex-grow overflow-hidden">
+				<textarea
+					class="bg h-full w-full resize-none bg-[#56545C] text-sm font-light text-white"
+					placeholder="Message"
+					rows="4"
+				/>
+				<button
+					class="rounded bg-[#56545C] py-[1px] px-1.5 text-xs text-white disabled:cursor-not-allowed disabled:opacity-75"
+					disabled
+					type="button"
+				>
+					Send
+				</button>
+			</form>
+		</div>
+		<div class="flex w-full gap-2" />
+	</div>
+</div>
