fe608fe59c437d9a9134920c169877f1bd0bf54e
Ethan Olsen <131eolsen@gmail.com>
2023-05-05 00:24:44 -0700

    fix: time formatting

--- src/lib/desktopTop/CurrentTime.svelte
+++ src/lib/desktopTop/CurrentTime.svelte
@@ -2,11 +2,11 @@
 	import { format } from 'date-fns';
 	import { onMount } from 'svelte';
 
-	let currentTime = format(new Date(), 'EEE MMM d h:mm b');
+	let currentTime = format(new Date(), 'EEE MMM d h:mm p');
 
 	onMount(() => {
 		setInterval(() => {
-			currentTime = format(new Date(), 'EEE MMM d h:mm b');
+			currentTime = format(new Date(), 'EEE MMM d h:mm p');
 		}, 1000);
 	});
 </script>
