2b8f9c9979df2b22dd42df19d92b44d7acc416ed
Ethan Olsen <131eolsen@gmail.com>
2023-06-13 18:45:00 -0700

    chore: add href tracking for css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	onMount(() => {
+		// add a data-href to body
+		window.document.body.setAttribute('data-href', window.location.pathname);
+		// add listener for url changes
+		window.addEventListener('popstate', () => {
+			window.document.body.setAttribute('data-href', window.location.pathname);
+		});
+	});
 </script>
 
 <main class="h-full w-full">
