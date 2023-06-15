8aec8e0f9595ae37e247c388ec4152c92d4c3456
Ethan Olsen <131eolsen@gmail.com>
2023-06-14 17:53:04 -0700

    chore: add metaball, add about anim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,9 @@
 	import TerminalBase from '$lib/terminal/TerminalBase.svelte';
 	import type { TerminalLine } from '$lib/terminal/terminal';
 	import { onMount } from 'svelte';
-	import { fly } from 'svelte/transition';
+	import { fade, fly } from 'svelte/transition';
 	import { intersectionObserver } from '$lib/actions/intersectionObserver';
 
-	let terminalHeight = 0;
 	let execute: (command: string) => void;
 	let lines: TerminalLine[] = [
 		{
@@ -46,19 +45,30 @@
 		}
 	];
 
+	let isMounted = false;
 	onMount(() => {
 		execute('help');
 		execute('ls');
+		isMounted = true;
 	});
 
 	let isAboutVisible = false;
+	$: canvasWidth = isMounted ? window.innerWidth : 0;
+	$: canvasHeight = isMounted ? window.innerHeight : 0;
 </script>
 
-<div class="h-full w-full bg-black font-roboto text-gray-50">
+<div class="relative h-0 w-0">
+	{#await import('$lib/metaballs/Metaballs.svelte') then Module}
+		<div class="rainbow absolute top-0 left-0" in:fade={{ duration: 1000 }}>
+			<Module.default width={canvasWidth} height={canvasHeight} />
+		</div>
+	{/await}
+</div>
+<div class="background-shift h-full w-full font-roboto text-gray-50">
 	<header class="flex w-full items-center justify-center">
 		<nav class="container flex p-4">
 			<div class="flex flex-grow items-center gap-2">
-				<a class="flex items-center justify-center gap-2 text-2xl" href="/"
+				<a class="z-50 flex items-center justify-center gap-2 text-2xl" href="/"
 					><svg
 						width="48"
 						height="48"
@@ -88,14 +98,20 @@
 				</a>
 			</div>
 			<div class="flex flex-grow items-center justify-end gap-2 text-lg">
-				<a class="opacity-75 transition-all hover:opacity-100 active:scale-95" href="#about">
+				<a
+					class="relative flex h-full items-center justify-center px-2 opacity-75 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:origin-center before:bg-current before:transition-all hover:opacity-100 hover:before:h-0.5 active:scale-95"
+					href="#about"
+				>
 					About
 				</a>
-				<a class="opacity-75 transition-all hover:opacity-100 active:scale-95" href="#projects">
+				<a
+					class="relative flex h-full items-center justify-center px-2 opacity-75 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:origin-center before:bg-current before:transition-all hover:opacity-100 hover:before:h-0.5 active:scale-95"
+					href="#projects"
+				>
 					Projects
 				</a>
 				<a
-					class="rounded bg-white px-3 py-2 text-black opacity-75 transition-all hover:opacity-100 active:scale-95"
+					class="flex h-full items-center justify-center rounded bg-white px-3 py-2 text-black opacity-75 transition-all hover:opacity-100 active:scale-95"
 					href="/app/desktop"
 				>
 					Open Desktop
@@ -103,47 +119,55 @@
 			</div>
 		</nav>
 	</header>
-	<div class="h-full w-full pt-9">
+	<div class="relative h-full w-full pt-9">
 		<div class="fly-up mx-auto w-fit px-4">
-			<h1 class="rainbow mx-auto max-w-2xl bg-clip-text text-center text-6xl font-bold">
+			<h1
+				style="filter: drop-shadow(2px 4px 0px black);"
+				class="rainbow mx-auto max-w-2xl bg-clip-text text-center text-6xl font-bold"
+			>
 				Full-Stack Developer With <span class="text-transparent">Passion</span>
 			</h1>
 		</div>
-		<div class="mx-auto mt-20 grid h-72 w-full max-w-2xl grid-cols-1 grid-rows-1 px-4">
+		<div class="mt-20 grid w-full grid-cols-1 grid-rows-1">
 			<div
 				style="grid-column: 1; grid-row: 1;"
-				class="rainbow aspect-square w-full rounded-3xl blur-md"
-			/>
-			<div
-				bind:clientHeight={terminalHeight}
-				style="grid-column: 1; grid-row: 1;"
-				class="relative z-10 flex aspect-square w-full flex-grow flex-col overflow-hidden rounded-3xl border border-[rgba(55,65,81,1)] bg-black pt-11"
+				class="relative mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 px-4"
 			>
 				<div
-					class="absolute left-0 top-0 z-10 flex w-full gap-2.5 border-b border-slate-700 bg-neutral-950 p-4"
-				>
-					<div class="h-2.5 w-2.5 rounded-full bg-red-500 opacity-50" />
-					<div class="h-2.5 w-2.5 rounded-full bg-yellow-500 opacity-50" />
-					<div class="h-2.5 w-2.5 rounded-full bg-green-500 opacity-50" />
-				</div>
-				<TerminalBase
-					style="font-size: clamp(0.25rem, 2vw, 0.75rem); line-height: calc(0.25rem + clamp(0.25rem, 2vw, 0.75rem));"
-					id="home"
-					bind:lines
-					bind:execute
+					style="grid-column: 1; grid-row: 1;"
+					class="rainbow aspect-square w-full rounded-3xl blur-md"
 				/>
+				<div
+					style="grid-column: 1; grid-row: 1;"
+					class="relative z-10 flex aspect-square w-full flex-grow flex-col overflow-hidden rounded-3xl border border-[rgba(55,65,81,1)] bg-black pt-11"
+				>
+					<div
+						class="absolute left-0 top-0 z-10 flex w-full gap-2.5 border-b border-slate-700 bg-neutral-950 p-4"
+					>
+						<div class="h-2.5 w-2.5 rounded-full bg-red-500 opacity-50" />
+						<div class="h-2.5 w-2.5 rounded-full bg-yellow-500 opacity-50" />
+						<div class="h-2.5 w-2.5 rounded-full bg-green-500 opacity-50" />
+					</div>
+					<TerminalBase
+						style="font-size: clamp(0.25rem, 2vw, 0.75rem); line-height: calc(0.25rem + clamp(0.25rem, 2vw, 0.75rem));"
+						id="home"
+						bind:lines
+						bind:execute
+					/>
+				</div>
+			</div>
+			<div style="grid-column: 1; grid-row: 1;" class="grid h-full w-full grid-rows-2">
+				<div />
+				<div class="h-full bg-gray-950" />
 			</div>
 		</div>
-		<div
-			style="padding-top: {terminalHeight ? terminalHeight - 200 : 350}px"
-			class="h-full w-full bg-gray-950"
-		>
+		<div class="-mt-1 h-full w-full overflow-hidden bg-gray-950">
 			<div
 				use:intersectionObserver={{ callback: () => (isAboutVisible = true) }}
 				class="prose prose-invert mx-auto max-w-2xl px-4 pt-12 prose-p:opacity-90"
 			>
 				<h2 class="mb-4 text-5xl">About</h2>
-				<div class="relative float-right my-2 mr-4 h-44 w-44">
+				<div class="relative my-2 h-44 w-44 md:float-right md:mr-4">
 					{#if isAboutVisible}
 						<img
 							in:fly={{ x: 100, delay: 500, duration: 1000 }}
@@ -164,13 +188,21 @@
 							alt=""
 						/>
 					{/if}
-					<img
-						class="absolute left-0 top-0 m-0 h-44 w-44 rounded-xl object-cover ring-4 ring-gray-950"
-						src={AboutPhoto}
-						alt="Ethan Olsen and Omen"
-						height="11rem"
-						width="11rem"
-					/>
+					<div
+						class:grayscale={!isAboutVisible}
+						class="absolute left-0 top-0 m-0 h-44 w-44 overflow-hidden rounded-xl object-cover ring-4 ring-gray-950 transition-all duration-500"
+					>
+						<img
+							class:scale-[400%]={!isAboutVisible}
+							class:-translate-y-24={!isAboutVisible}
+							class:translate-x-20={!isAboutVisible}
+							class="m-0 h-44 w-44 object-cover object-center transition-all duration-1000"
+							src={AboutPhoto}
+							alt="Ethan Olsen and Omen"
+							height="11rem"
+							width="11rem"
+						/>
+					</div>
 				</div>
 				<p>
 					I am a full stack cross platform web developer with a focus on developing internal
@@ -200,6 +232,17 @@
 		100% {
 			background-position: 0% 50%;
 		}
+	}
+	@keyframes background-shift {
+		0% {
+			background-position: 0% 0%;
+		}
+		100% {
+			background-position: 0% 100%;
+		}
+	}
+	.background-shift {
+		animation: background-shift 2s linear infinite;
 	}
 	:global(html) {
 		--rainbow-gradient: linear-gradient(
