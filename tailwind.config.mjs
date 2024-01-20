eb01187e9b20a4b4f97bb3345a6f9353e6869cd6
Ethan Olsen <131eolsen@gmail.com>
2024-01-20 12:12:12 -0800

    chore: add tailwind plugins

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			scrollbar: ["rounded"],
+		},
+		fontFamily: {
+			mono: ["Roboto Mono", "monospace"],
+			roboto: ["Roboto", "sans-serif"],
+			cmd: ["VT323", "monospace"],
+		},
 	},
-	plugins: [],
-}
+
+	plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
+	variants: {},
+};
