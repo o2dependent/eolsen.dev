9749eb0bc3c9f2c7241ec3efc3ed7934243843cb
Ethan Olsen <131eolsen@gmail.com>
2024-01-21 11:51:45 -0800

    feat: blog and project list view

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,7 +3,10 @@
 	export let isHome = false;
 </script>
 
-<header bind:clientHeight={navHeight} class="relative z-50 flex w-full items-center justify-center">
+<header
+	bind:clientHeight={navHeight}
+	class="relative z-50 flex w-full items-center justify-center"
+>
 	<nav class="container flex px-4">
 		<div class="flex flex-grow items-center gap-2">
 			<a
@@ -29,9 +32,15 @@
 						<circle cx="23.3438" cy="24" r="19.3125" fill="#FEFEFE" />
 					</mask>
 					<g mask="url(#mask0_6476_541)">
-						<path d="M7.5 7.96875H41.1562V12.6562H7.5V7.96875Z" fill="#111111" />
+						<path
+							d="M7.5 7.96875H41.1562V12.6562H7.5V7.96875Z"
+							fill="#111111"
+						/>
 						<path d="M7.5 35.0625H41.1562V39.75H7.5V35.0625Z" fill="#111111" />
-						<path d="M2.34375 22.0312H24.75V26.7188H2.34375V22.0312Z" fill="#111111" />
+						<path
+							d="M2.34375 22.0312H24.75V26.7188H2.34375V22.0312Z"
+							fill="#111111"
+						/>
 					</g>
 				</svg>
 				<span>Ethan Olsen</span>
@@ -40,15 +49,21 @@
 		<div class="flex flex-grow items-center justify-end gap-2 text-lg">
 			<a
 				class="relative flex h-full items-center justify-center px-2 py-4 opacity-75 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:origin-center before:bg-current before:transition-all hover:opacity-100 hover:before:h-0.5"
-				href={isHome ? '#about' : '/#about'}
+				href={isHome ? "#about" : "/#about"}
 			>
 				About
 			</a>
 			<a
 				class="relative flex h-full items-center justify-center px-2 py-4 opacity-75 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:origin-center before:bg-current before:transition-all hover:opacity-100 hover:before:h-0.5"
-				href={isHome ? '#projects' : '/#projects'}
+				href="/project"
 			>
 				Projects
+			</a>
+			<a
+				class="relative flex h-full items-center justify-center px-2 py-4 opacity-75 transition-all before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:origin-center before:bg-current before:transition-all hover:opacity-100 hover:before:h-0.5"
+				href="/blog"
+			>
+				Blogs
 			</a>
 			<a
 				class="my-4 flex items-center justify-center rounded bg-white px-3 py-2 text-black opacity-75 transition-all hover:opacity-100 active:scale-95"
