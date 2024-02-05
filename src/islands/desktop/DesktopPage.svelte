1c2277999b10dbde0a21bf9f094e261817524fc2
Ethan Olsen <131eolsen@gmail.com>
2024-02-04 19:33:04 -0800

    feat(desktop): init

---
+++ src/islands/desktop/DesktopPage.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import BiosBootScreen from "$lib/biosBootScreen/BiosBootScreen.svelte";
+	import { onMount } from "svelte";
+
+	let loaded = false;
+
+	onMount(() => {
+		window.document.body.setAttribute("data-href", window.location.pathname);
+	});
+</script>
+
+{#await import("$lib/Desktop.svelte") then Module}
+	<Module.default bind:loaded />
+{/await}
+<!-- <Desktop bind:loaded /> -->
+<BiosBootScreen {loaded} />
+<!-- <BootScreen /> -->
