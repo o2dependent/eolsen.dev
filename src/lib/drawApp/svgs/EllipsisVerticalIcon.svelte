0d6d88e44c09cd0c91962d414c79a17e48602947
Ethan Olsen <131eolsen@gmail.com>
2024-02-06 12:37:15 -0800

    chore: add icons

---
+++ src/lib/drawApp/svgs/EllipsisVerticalIcon.svelte
@@ -0,0 +1,12 @@
+<svg
+	width="18px"
+	height="18px"
+	fill="currentColor"
+	xmlns="http://www.w3.org/2000/svg"
+	class="ionicon"
+	viewBox="0 0 512 512"
+>
+	<circle cx="256" cy="256" r="48" />
+	<circle cx="256" cy="416" r="48" />
+	<circle cx="256" cy="96" r="48" />
+</svg>
