c8ccd8ec0538eac8ba18e2d83f021cc612adb436
Ethan Olsen <131eolsen@gmail.com>
2024-02-06 12:37:23 -0800

    chore: remove log

--- src/lib/desktopApps/DesktopApps.svelte
+++ src/lib/desktopApps/DesktopApps.svelte
@@ -1,28 +1,24 @@
 <script lang="ts">
-	import AboutApp from '$lib/aboutApp/AboutApp.svelte';
-	import Blog from '$lib/blog/Blog.svelte';
-	import Draw from '$lib/drawApp/Draw.svelte';
+	import AboutApp from "$lib/aboutApp/AboutApp.svelte";
+	import Blog from "$lib/blog/Blog.svelte";
+	import Draw from "$lib/drawApp/Draw.svelte";
 
-	import Project from '$lib/project/Project.svelte';
+	import Project from "$lib/project/Project.svelte";
 
-	import Terminal from '$lib/terminal/Terminal.svelte';
-	import { apps } from '$stores/apps.store';
-
-	$: {
-		console.log({ $apps });
-	}
+	import Terminal from "$lib/terminal/Terminal.svelte";
+	import { apps } from "$stores/apps.store";
 </script>
 
 {#each $apps as appWindow (appWindow.id)}
-	{#if appWindow.name === 'Terminal'}
+	{#if appWindow.name === "Terminal"}
 		<Terminal {appWindow} />
-	{:else if appWindow.name === 'Project'}
+	{:else if appWindow.name === "Project"}
 		<Project {appWindow} />
-	{:else if appWindow.name === 'About Site'}
+	{:else if appWindow.name === "About Site"}
 		<AboutApp {appWindow} />
-	{:else if appWindow.name === 'Pixel Paint'}
+	{:else if appWindow.name === "Pixel Paint"}
 		<Draw {appWindow} />
-	{:else if appWindow.name === 'Blog'}
+	{:else if appWindow.name === "Blog"}
 		<Blog {appWindow} />
 	{/if}
 {/each}
