b3ee8ca06fcbeaf34f6ee8dd0791f31033627fca
Ethan Olsen <131eolsen@gmail.com>
2024-02-09 11:56:26 -0800

    feat: magnetic hover effect

--- src/components/PostCard.svelte
+++ src/components/PostCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { magneticHover } from "./../utils/magneticHover.ts";
 	import FormattedDate from "./FormattedDate.svelte";
 
 	export let image: string | undefined;
@@ -6,13 +7,49 @@
 	export let title: string;
 	export let pubDate: Date;
 	export let isFull: boolean = false;
+
+	let hoverParent: HTMLElement;
+	let imageBoundingRect: DOMRect;
+	$: initTranslate = {
+		x: imageBoundingRect?.width * -0.5,
+		y: imageBoundingRect?.height * (-2 / 3),
+	};
+	$: console.log(imageBoundingRect);
 </script>
 
-<li class={`flex items-center justify-center ${isFull ? "md:col-span-2" : ""}`}>
-	<a class={`group mx-auto w-fit ${isFull ? "md:text-center" : ""}`} {href}>
+<li
+	class={`relative max-w-fit mx-auto group flex items-center justify-center ${
+		isFull ? "md:col-span-2" : ""
+	}`}
+>
+	<div
+		class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex items-center justify-center w-full h-5/6 transition-all"
+	>
+		<img
+			class="group-hover:rounded-3xl rounded-2xl border-2 blur-xl opacity-15 w-full h-full ease-in-out -mb-full"
+			src={image ?? `/blog_placeholder${(title?.length % 4) + 1}-.jpg`}
+			alt=""
+			bind:contentRect={imageBoundingRect}
+			use:magneticHover={{
+				strength: { x: 0.25, y: 0.25 },
+				hoverParent: hoverParent,
+				duration: 4,
+				opacity: { hover: 0.5, init: 0.15, duration: 2 },
+				scale: { hover: 1.1, init: 1, duration: 1 },
+			}}
+		/>
+	</div>
+	<a
+		{href}
+		use:magneticHover={{
+			strength: { x: isFull ? 0.1 : 0.15, y: isFull ? 0.1 : 0.15 },
+		}}
+		bind:this={hoverParent}
+		class={`z-10 mx-auto w-fit ${isFull ? "md:text-center" : ""}`}
+	>
 		<img
 			style="grid-area: 1 / 1"
-			class="rounded-xl border-2 border-black/5 z-10 group-hover:-translate-y-1 transition-transform duration-500 ease-in-out"
+			class="rounded-xl border-2 border-black/5 z-10"
 			width={720}
 			height={360}
 			src={image ?? `/blog_placeholder${(title?.length % 4) + 1}-.jpg`}
---
+++ src/utils/magneticHover.ts
@@ -0,0 +1,104 @@
+import gsap from "gsap";
+import type { Action } from "svelte/action";
+
+interface Options {
+	strength?: { x: number | undefined; y: number | undefined };
+	duration?: number | undefined;
+	hoverParent?: HTMLElement;
+	initTranslate?: { x: number; y: number };
+	opacity?: { init: number; hover: number; duration: number };
+	scale?: { init: number; hover: number; duration: number };
+}
+
+export const magneticHover: Action<HTMLElement, Options> = (
+	node,
+	{ strength, duration, initTranslate, hoverParent, opacity, scale } = {
+		strength: { x: 0.3, y: 0.3 },
+		duration: 0.8,
+		initTranslate: { x: 0, y: 0 },
+	},
+) => {
+	if (!duration) duration = 0.8;
+	if (!strength) strength = { x: 0.3, y: 0.3 };
+	if (!strength?.x) strength.x = 0.3;
+	if (!strength?.y) strength.y = 0.3;
+	if (!hoverParent) hoverParent = node;
+	if (!initTranslate) initTranslate = { x: 0, y: 0 };
+
+	let boundingRect = node.getBoundingClientRect();
+	const resizeWindow = () => {
+		boundingRect = node.getBoundingClientRect();
+	};
+	const mouseleave = () => {
+		// Animate the node back to its original position
+		gsap.to(node, {
+			x: initTranslate?.x!, // Reset horizontal position
+			y: initTranslate?.y!, // Reset vertical position
+			duration,
+			ease: "elastic.out(1,0.3)", // Easing function for a 'springy' return
+		});
+		if (opacity)
+			gsap.to(node, {
+				opacity: opacity?.init,
+				duration: opacity?.duration,
+				ease: "power3.out",
+			});
+	};
+	const mousemove = (e: MouseEvent) => {
+		// Calculate mouse position relative to the node
+		const mousePosX = e.pageX - boundingRect.left;
+		const mousePosY = e.pageY - boundingRect.top;
+
+		// This creates a 'magnetic' effect where the node moves towards the mouse
+		gsap.to(node, {
+			x: (mousePosX - boundingRect.width / 2) * strength!.x!, // Move horizontally towards mouse
+			y: (mousePosY - boundingRect.height / 2) * strength!.y!, // Move vertically towards mouse
+			duration, // Duration of the animation
+			ease: "power3.out", // Easing function for smooth animation
+		});
+	};
+
+	const mouseenter = () => {
+		if (opacity)
+			gsap.to(node, {
+				opacity: opacity?.hover,
+				duration: opacity?.duration,
+				ease: "power3.out",
+			});
+	};
+
+	hoverParent.addEventListener("mouseleave", mouseleave);
+	hoverParent.addEventListener("mouseenter", mouseenter);
+	window.addEventListener("resize", resizeWindow);
+	hoverParent.addEventListener("mousemove", mousemove);
+	return {
+		update(updatedOptions) {
+			console.log(`update`, hoverParent);
+			hoverParent!.removeEventListener("mouseleave", mouseleave);
+			hoverParent!.removeEventListener("mouseenter", mouseenter);
+			hoverParent!.removeEventListener("mousemove", mousemove);
+
+			strength = updatedOptions?.strength
+				? updatedOptions?.strength
+				: { x: 0.3, y: 0.3 };
+			duration = updatedOptions?.duration ? updatedOptions?.duration : 0.8;
+			initTranslate = updatedOptions?.initTranslate
+				? updatedOptions?.initTranslate
+				: { x: 0, y: 0 };
+			hoverParent = updatedOptions?.hoverParent
+				? updatedOptions?.hoverParent
+				: node;
+
+			hoverParent.addEventListener("mouseleave", mouseleave);
+			hoverParent.addEventListener("mouseenter", mouseenter);
+			window.addEventListener("resize", resizeWindow);
+			hoverParent.addEventListener("mousemove", mousemove);
+		},
+		destroy() {
+			hoverParent!.removeEventListener("mouseleave", mouseleave);
+			hoverParent!.removeEventListener("mouseenter", mouseenter);
+			window.removeEventListener("resize", resizeWindow);
+			hoverParent!.removeEventListener("mousemove", mousemove);
+		},
+	};
+};
