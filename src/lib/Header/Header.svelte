be3f0a069c819a81eee09e7c579c60b6907b85da
Ethan Olsen <131eolsen@gmail.com>
2024-02-10 13:01:22 -0800

    feat: mobile header

--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -1,93 +1,11 @@
 <script lang="ts">
-	import StickyLink from "$components/StickyLink.svelte";
+	import FullHeader from "./FullHeader.svelte";
+	import MobileHeader from "./MobileHeader.svelte";
 	export let navHeight = 0;
 	export let isHome = false;
 </script>
 
-<header
-	bind:clientHeight={navHeight}
-	class="relative z-50 hidden md:flex w-full items-center justify-center"
->
-	<nav class="container flex px-4">
-		<div class="flex flex-grow items-center gap-2">
-			<StickyLink
-				mag={{
-					strength: { x: 0.3, y: 0.3 },
-				}}
-				class="relative px-1 rounded-lg z-50 flex items-center justify-center gap-2 py-4 text-2xl transition-opacity hover:opacity-100"
-				href="/"
-			>
-				<svg
-					width="48"
-					height="48"
-					viewBox="0 0 48 48"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<circle cx="24" cy="24" r="24" fill="#FAFAFA" />
-					<mask
-						id="mask0_6476_541"
-						style="mask-type:alpha"
-						maskUnits="userSpaceOnUse"
-						x="4"
-						y="4"
-						width="39"
-						height="40"
-					>
-						<circle cx="23.3438" cy="24" r="19.3125" fill="#FEFEFE" />
-					</mask>
-					<g mask="url(#mask0_6476_541)">
-						<path
-							d="M7.5 7.96875H41.1562V12.6562H7.5V7.96875Z"
-							fill="#111111"
-						/>
-						<path d="M7.5 35.0625H41.1562V39.75H7.5V35.0625Z" fill="#111111" />
-						<path
-							d="M2.34375 22.0312H24.75V26.7188H2.34375V22.0312Z"
-							fill="#111111"
-						/>
-					</g>
-				</svg>
-				<span>Ethan Olsen</span>
-			</StickyLink>
-		</div>
-		<div class="flex flex-grow items-center justify-end gap-2 text-lg">
-			<StickyLink
-				mag={{
-					strength: { x: 0.3, y: 0.3 },
-				}}
-				class="relative rounded-lg flex h-full items-center justify-center px-2 py-4 opacity-75 transition-opacity hover:opacity-100"
-				href={isHome ? "#about" : "/#about"}
-			>
-				About
-			</StickyLink>
-			<StickyLink
-				mag={{
-					strength: { x: 0.3, y: 0.3 },
-				}}
-				class="relative rounded-lg flex h-full items-center justify-center px-2 py-4 opacity-75 transition-opacity hover:opacity-100"
-				href="/project"
-			>
-				Projects
-			</StickyLink>
-			<StickyLink
-				mag={{
-					strength: { x: 0.3, y: 0.3 },
-				}}
-				class="relative rounded-lg flex h-full items-center justify-center px-2 py-4 opacity-75 transition-opacity hover:opacity-100"
-				href="/blog"
-			>
-				Blogs
-			</StickyLink>
-			<StickyLink
-				mag={{
-					strength: { x: 0.3, y: 0.3 },
-				}}
-				class="my-4 flex items-center justify-center rounded bg-white px-3 py-2 text-black opacity-75 transition-colors hover:bg-white/10 hover:text-white active:ring active:ring-white hover:opacity-100 active:scale-95"
-				href="/desktop"
-			>
-				Open Desktop
-			</StickyLink>
-		</div>
-	</nav>
+<header bind:clientHeight={navHeight} class="relative z-50 w-full">
+	<FullHeader {isHome} />
+	<MobileHeader {navHeight} />
 </header>
---
+++ src/lib/Header/MobileHeader.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+	import gsap from "gsap";
+	import HamburgerButton from "./HamburgerButton.svelte";
+
+	let active = false;
+	let menu: HTMLElement;
+	let menuItems: HTMLElement[] = [];
+	let showMenu = false;
+
+	$: {
+		if (typeof window !== "undefined") {
+			if (active) {
+				showMenu = true;
+				for (let i = 0; i < menuItems.length; i++) {
+					if (!menuItems?.[i]) continue;
+					gsap.fromTo(
+						menuItems[i],
+						{ x: -100, opacity: 0 },
+						{
+							x: 0,
+							opacity: 1,
+							duration: 0.5,
+							delay: i * 0.1 + 0.25,
+							ease: "power3.out",
+						},
+					);
+				}
+				gsap.to(menu, {
+					backgroundColor: "rgb(0 0 0 / 0.75)",
+					"--tw-backdrop-blur": "blur(16px)",
+					duration: 0.25 + menuItems.length * 0.1,
+				});
+			} else {
+				for (let i = 0; i < menuItems.length; i++) {
+					if (!menuItems?.[i]) continue;
+					gsap.fromTo(
+						menuItems[i],
+						{ x: 0, opacity: 1 },
+						{
+							x: -100,
+							opacity: 0,
+							duration: 0.3,
+							delay: i * 0.1,
+							ease: "power3.out",
+						},
+					);
+				}
+				gsap
+					.to(menu, {
+						backgroundColor: "rgb(0 0 0 / 0)",
+						"--tw-backdrop-blur": "blur(0px)",
+						duration: menuItems.length * 0.1 + 0.3,
+					})
+					.eventCallback("onComplete", () => {
+						showMenu = false;
+					});
+			}
+		}
+	}
+</script>
+
+<nav class="flex h-16 md:hidden w-full items-center justify-end sticky top-0">
+	<HamburgerButton bind:active />
+	<div
+		bind:this={menu}
+		class:flex={showMenu}
+		class:hidden={!showMenu}
+		class="fixed top-0 left-0 w-full pt-20 h-full bg-black/75 backdrop-blur-lg z-40 px-4 flex flex-col gap-4 overflow-hidden"
+	>
+		<a
+			on:click={() => (active = false)}
+			class="menu-item"
+			bind:this={menuItems[0]}
+			href="/#about">About</a
+		>
+		<a
+			on:click={() => (active = false)}
+			class="menu-item"
+			bind:this={menuItems[1]}
+			href="/project">Projects</a
+		>
+		<a
+			on:click={() => (active = false)}
+			class="menu-item"
+			bind:this={menuItems[2]}
+			href="/blog">Blog</a
+		>
+		<a
+			on:click={() => (active = false)}
+			bind:this={menuItems[3]}
+			class="fixed bottom-4 left-4 flex gap-4 items-center text-xl"
+			href="/"
+		>
+			<svg
+				class="h-12 w-12"
+				viewBox="0 0 48 48"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="24" cy="24" r="24" fill="#FAFAFA" />
+				<mask
+					id="mask0_6476_542"
+					style="mask-type:alpha"
+					maskUnits="userSpaceOnUse"
+					x="4"
+					y="4"
+					width="39"
+					height="40"
+				>
+					<circle cx="23.3438" cy="24" r="19.3125" fill="#FEFEFE" />
+				</mask>
+				<g mask="url(#mask0_6476_542)">
+					<path d="M7.5 7.96875H41.1562V12.6562H7.5V7.96875Z" fill="#111111" />
+					<path d="M7.5 35.0625H41.1562V39.75H7.5V35.0625Z" fill="#111111" />
+					<path
+						d="M2.34375 22.0312H24.75V26.7188H2.34375V22.0312Z"
+						fill="#111111"
+					/>
+				</g>
+			</svg>
+			<span>Ethan Olsen</span>
+		</a>
+	</div>
+</nav>
+
+<style lang="postcss">
+	.menu-item {
+		@apply text-3xl;
+	}
+</style>
