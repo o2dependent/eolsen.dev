45a06b7a99c88dd5f8932c33534c6de135168187
Ethan Olsen <131eolsen@gmail.com>
2024-11-16 11:17:11 -0800

    feat(models): tv with screen

--- src/lib/vhs/VHSApp.ts
+++ src/lib/vhs/VHSApp.ts
@@ -12,9 +12,15 @@ import {
 	Color4,
 	VideoRecorder,
 	SceneLoader,
+	type ISceneLoaderAsyncResult,
+	MeshBuilder,
+	Material,
+	StandardMaterial,
+	Color3,
 } from "@babylonjs/core";
 import * as _ from "@babylonjs/loaders/glTF/2.0";
 import type Meyda from "meyda";
+import { lerp } from "$lib/math";
 
 export class VHSApp {
 	nodes: any[] = [];
@@ -33,6 +39,8 @@ export class VHSApp {
 	flashLight: PointLight | null = null;
 	canvas: HTMLCanvasElement;
 	seizureMode: boolean = false;
+
+	vhs: ISceneLoaderAsyncResult | null = null;
 
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
@@ -88,13 +96,18 @@ export class VHSApp {
 		// );
 		// this.nodes.push(model);
 
-		const vhs = await SceneLoader.LoadAsync(
-			"http://localhost:4321/models/vhs_ps1/",
+		const vhs = await SceneLoader.ImportMeshAsync(
+			"Object_4",
+			"http://localhost:4321/models/psx_old_tv/",
 			"scene.gltf",
 		);
 		this.nodes.push(vhs);
-		const nodes = vhs.getNodes();
-		console.log(nodes);
+		vhs.meshes[0].scaling = new Vector3(8, 8, 8);
+		vhs.meshes[0].rotate(new Vector3(0, 1, 0), -Math.PI / 2);
+		this.vhs = vhs;
+
+		// const nodes = vhs.getNodes();
+		// console.log(nodes);
 
 		// hide/show the Inspector
 		if (import.meta.env.MODE === "development") {
@@ -131,6 +144,41 @@ export class VHSApp {
 
 		this.engine = engine;
 		this.scene = scene;
+
+		// let start = NaN;
+		// const rotateFull = (dt: number) => {
+		// 	if (Number.isNaN(start)) start = dt;
+		// 	const t = dt - start;
+		// 	const x = lerp(Math.PI / 2, 0, t / 2000);
+		// 	const y = lerp(0, Math.PI / 2, t / 2000);
+		// 	const z = lerp(-Math.PI / 4, 0, t / 2000);
+		// 	vhs.meshes[0].rotation = new Vector3(x, y, z);
+		// 	console.log(t);
+		// 	if (t < 2000) requestAnimationFrame(rotateFull);
+		// 	else vhs.meshes[0].rotation = new Vector3(0, Math.PI / 2, 0);
+		// };
+		// requestAnimationFrame(rotateFull);
+
+		const size = 0.125;
+		for (let i = 0; i <= (2 / size) * 2; i++) {
+			for (let j = 0; j < (2 / size) * 2; j++) {
+				const box = MeshBuilder.CreateBox("box", {
+					height: size,
+					width: size,
+					depth: 0.125,
+				});
+				box.rotation = new Vector3(0, Math.PI / 2, 0);
+				box.position = new Vector3(1.9, 2.2 - j * size, 2 - i * size);
+
+				const material = new StandardMaterial(`material_${i}_${j}`, scene);
+				material.emissiveColor = new Color3(
+					i / ((2 / size) * 2),
+					j / ((2 / size) * 2),
+					(j + i) / ((2 / size) * 4),
+				);
+				box.material = material;
+			}
+		}
 	}
 
 	dispose() {
